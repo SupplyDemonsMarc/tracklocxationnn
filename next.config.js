1e4eb545d4b9098fe5a80089c91fec70b887f1e7
SupplyDemonsMarc <142413801+SupplyDemonsMarc@users.noreply.github.com>
2026-06-30 05:24:45 -0700

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  env: {
+    MONGODB_URI: process.env.MONGODB_URI,
+    JWT_SECRET: process.env.JWT_SECRET,
+    ADMIN_USERNAME: process.env.ADMIN_USERNAME,
+    ADMIN_PASSWORD: process.env.ADMIN_PASSWORD,
+  }
+};
+
+module.exports = nextConfig;
